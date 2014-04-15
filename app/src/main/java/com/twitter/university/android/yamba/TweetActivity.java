0fa436a3633d5c39ab6af2753b77cf028a650a70
G. Blake Meike <bmeike@twitter.com>
2014-04-15 15:45:11 -0700

    Working count

--- app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
+++ app/src/main/java/com/twitter/university/android/yamba/TweetActivity.java
@@ -1,15 +1,72 @@
 package com.twitter.university.android.yamba;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class TweetActivity extends Activity {
     private static final String TAG = "TWEET";
 
+    private int okColor;
+    private int warnColor;
+    private int errColor;
+
+    private int tweetLenMax;
+    private int warnMax;
+    private int errMax;
+
+    private EditText tweetView;
+    private TextView countView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet);
+
+        Resources rez = getResources();
+        okColor = rez.getColor(R.color.green);
+        tweetLenMax = rez.getInteger(R.integer.tweet_limit);
+        warnColor = rez.getColor(R.color.yellow);
+        warnMax = rez.getInteger(R.integer.warn_limit);
+        errColor = rez.getColor(R.color.red);
+        errMax = rez.getInteger(R.integer.err_limit);
+        countView = (TextView) findViewById(R.id.tweet_count);
+
+        tweetView = (EditText) findViewById(R.id.tweet_tweet);
+        tweetView.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void afterTextChanged(Editable s) { updateCount(); }
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int b, int n, int a) { }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int b, int p, int n) { }
+                });
+
+    }
+
+    void updateCount() {
+        int n = tweetView.getText().length();
+
+        n = tweetLenMax - n;
+
+        int color;
+        if (n > warnMax) { color = okColor; }
+        else if (n > errMax) { color = warnColor; }
+        else  { color = errColor; }
+
+        countView.setText(String.valueOf(n));
+        countView.setTextColor(color);
+    }
+
+    private boolean checkTweetLen(int n) {
+        return (errMax < n) && (tweetLenMax > n);
     }
 }
