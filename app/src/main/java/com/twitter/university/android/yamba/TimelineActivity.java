db49dcf73e06c77fb99026dda6f94c6280be567d
G. Blake Meike <bmeike@twitter.com>
2014-04-17 14:59:02 -0700

    Timeline Fragment

--- app/src/main/java/com/twitter/university/android/yamba/TimelineActivity.java
+++ app/src/main/java/com/twitter/university/android/yamba/TimelineActivity.java
@@ -18,73 +18,11 @@ import android.widget.TextView;
 import com.twitter.university.android.yamba.service.YambaContract;
 
 
-public class TimelineActivity
-        extends ListActivity
-        implements LoaderManager.LoaderCallbacks<Cursor>
-{
-    private static final int TIMELINE_LOADER = 42;
-
-    private static final String[] PROJ = new String[] {
-        YambaContract.Timeline.Columns.HANDLE,
-        YambaContract.Timeline.Columns.TIMESTAMP,
-        YambaContract.Timeline.Columns.TWEET
-    };
-
-    private static final int[] VIEWS = new int[] {
-        R.id.timeline_handle,
-        R.id.timeline_timestamp,
-        R.id.timeline_tweet
-    };
-
-    static class TimelineBinder implements SimpleCursorAdapter.ViewBinder {
-
-        @Override
-        public boolean setViewValue(View v, Cursor c, int col) {
-            if (R.id.timeline_timestamp != v.getId()) { return false; }
-
-            CharSequence s = "long ago";
-            long t = c.getLong(col);
-            if (0 < t) { s = DateUtils.getRelativeTimeSpanString(t); }
-            ((TextView) v).setText(s);
-            return true;
-        }
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(
-            this,
-            YambaContract.Timeline.URI,
-            null,
-            null,
-            null,
-            YambaContract.Timeline.Columns.TIMESTAMP + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor c) {
-        ((SimpleCursorAdapter) getListAdapter()).swapCursor(c);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-        ((SimpleCursorAdapter) getListAdapter()).swapCursor(null);
-    }
+public class TimelineActivity extends YambaActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-            this,
-            R.layout.timeline_row,
-            null,
-            PROJ,
-            VIEWS,
-            0);
-        adapter.setViewBinder(new TimelineBinder());
-        setListAdapter(adapter);
-
-        getLoaderManager().initLoader(TIMELINE_LOADER, null, this);
+        setContentView(R.layout.activity_timeline);
     }
 }
---
+++ app/src/main/java/com/twitter/university/android/yamba/TimelineFragment.java
@@ -0,0 +1,94 @@
+package com.twitter.university.android.yamba;
+
+import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import com.twitter.university.android.yamba.service.YambaContract;
+
+/**
+ * Created by bmeike on 4/17/14.
+ */
+public class TimelineFragment
+        extends ListFragment
+        implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    private static final int TIMELINE_LOADER = 42;
+
+    private static final String[] PROJ = new String[] {
+            YambaContract.Timeline.Columns.HANDLE,
+            YambaContract.Timeline.Columns.TIMESTAMP,
+            YambaContract.Timeline.Columns.TWEET
+    };
+
+    private static final int[] VIEWS = new int[] {
+            R.id.timeline_handle,
+            R.id.timeline_timestamp,
+            R.id.timeline_tweet
+    };
+
+    static class TimelineBinder implements SimpleCursorAdapter.ViewBinder {
+
+        @Override
+        public boolean setViewValue(View v, Cursor c, int col) {
+            if (R.id.timeline_timestamp != v.getId()) { return false; }
+
+            CharSequence s = "long ago";
+            long t = c.getLong(col);
+            if (0 < t) { s = DateUtils.getRelativeTimeSpanString(t); }
+            ((TextView) v).setText(s);
+            return true;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //View v = inflater.inflate(R.layout.activity_tweet, container, false);
+        //v.findViewById(R.id.tweet_label);
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.timeline_row,
+                null,
+                PROJ,
+                VIEWS,
+                0);
+        adapter.setViewBinder(new TimelineBinder());
+        setListAdapter(adapter);
+
+        getLoaderManager().initLoader(TIMELINE_LOADER, null, this);
+
+        return v;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(
+                getActivity(),
+                YambaContract.Timeline.URI,
+                null,
+                null,
+                null,
+                YambaContract.Timeline.Columns.TIMESTAMP + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor c) {
+        ((SimpleCursorAdapter) getListAdapter()).swapCursor(c);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        ((SimpleCursorAdapter) getListAdapter()).swapCursor(null);
+    }
+}
